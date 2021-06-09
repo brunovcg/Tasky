9629f1f01a761d1b262a4711dc3369b2521a251f
brunogouveia <brunovcg@gmail.com>
2021-06-09 18:16:04 -0300

    feature/button: added 'setSize' prop to choose between small, medium, large

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const MyButton = styled.button`
-    width: 90px;
+    width: ${props=> {if(props.setSize === "large") {return "90px"}     
+        else if (props.setSize === "medium") { return "70px"} 
+        else {return "50px"}
+    }};
     height: 30px;
     background-color: ${props=>props.setColor};
     color: white;
@@ -9,7 +12,10 @@ const MyButton = styled.button`
     border-radius: 5px;
     margin: 5px;
     font-weight: bold;
-    font-size: 0.8rem;
+    font-size: ${props=> {if(props.setSize === "large") {return "0.8rem"}     
+        else if (props.setSize === "medium") { return "0.7rem"} 
+        else {return "0.5rem"}
+    }};
     box-shadow: 1px 1px 1px rgba(0,0,0,0.3);
     transition: 0.2s;
     :hover{
--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -1,5 +1,6 @@
 import NotFound from '../../assets/page-not-found.png';
 import {Container, Info} from './styles';
+import Button from '../../components/button/Button'
 
 const PageNotFound = () => {
     return (
@@ -10,6 +11,31 @@ const PageNotFound = () => {
                 <h3>We couldn't find that page!</h3>
                 <h2>Page Not Found</h2>
                 <h1>Tasky</h1>
+
+                <Button 
+                    setColor={"var(--red)"}
+                    setSize={"small"}
+                    click={()=>console.log("small")}                    
+                    >small
+                </Button>
+
+                <Button 
+                    setColor={"var(--red)"}
+                    setSize={"medium"}
+                    click={()=>console.log("small")}                    
+                    >medium
+                </Button>
+
+                <Button 
+                    setColor={"var(--red)"}
+                    setSize={"large"}
+                    click={()=>console.log("large")}                    
+                    >large
+                </Button>
+
+
+
+
             </Info>
         </Container>
     )
