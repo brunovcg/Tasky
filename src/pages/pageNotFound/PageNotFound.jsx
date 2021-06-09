b9f2755c1642e426304c7b0d6982e1438ceb3458
Carlos Martorini <c7.silveira@gmail.com>
2021-06-09 13:53:08 -0300

    feat: add page not found

--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -1,12 +1,16 @@
 import NotFound from '../../assets/page-not-found.png';
-import {Container} from './styles';
+import {Container, Info} from './styles';
 
 const PageNotFound = () => {
     return (
         <Container>
-        <img src={NotFound} alt='Page NOT Found'/>
-        <h2>Page NOT Found</h2>
-        <h1>Tasky</h1>
+            <img src={NotFound} alt='Page NOT Found'/>
+            <Info>
+                <h3>Woops... Looks like nothing is here!</h3>
+                <h3>We couldn't find that page!</h3>
+                <h2>Page Not Found</h2>
+                <h1>Tasky</h1>
+            </Info>
         </Container>
     )
 }
