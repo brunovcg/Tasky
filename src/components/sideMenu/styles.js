c87d8d7988a1d0a0ced4d61ac2b0bcd78b4c1633
Carlos Martorini <c7.silveira@gmail.com>
2021-06-09 22:06:55 -0300

    feat: add button logout

--- src/components/sideMenu/styles.js
+++ src/components/sideMenu/styles.js
@@ -7,11 +7,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    p {
-        margin-bottom: 1.5rem;
-        color: var(--white);
-    }
+    justify-content: space-between;
 
     span {
         color: var(--white);
@@ -29,17 +25,11 @@ export const Container = styled.div`
 `;
 
 export const UserContainer = styled.div`
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
-    background-color: #dcdcdc;
-    margin-top: 2rem;
-    margin-bottom: 1rem;
+    text-align: center;
 
-    img {
-        width: 4rem;
-        height: 4rem;
-        padding: 0.3rem;
+    p {
+        text-align: center;
+        color: var(--white);
     }
 
     @media (max-width: 500px) {
@@ -48,22 +38,36 @@ export const UserContainer = styled.div`
 
 `;
 
+export const Img = styled.img`
+    width: 4rem;
+    height: 4rem;
+    padding: 0.3rem;
+    border-radius: 50%;
+    background-color: #dcdcdc;
+    margin-top: 2rem;
+    align-items: center;
+`;
+
 export const LinkContainer = styled.div`
     display: flex;
     flex-direction: column;
+    margin-top: 1.5rem;
 `;
 
 export const Link = styled.a`
-    width: 9vw;
+    width: 7vw;
     height: 3vh;
     border-bottom: 1px solid var(--white);
     color: var(--white);
     font-size: 1.2rem;
-    text-align: center;
     margin: 1rem;
 
     :hover {
         font-weight: bold;
         border-bottom: 2px solid var(--white);
     }
+`;
+
+export const ButtonContainer = styled.div`
+    margin-bottom: 1rem;
 `;
--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import NotFound from '../../assets/page-not-found.png';
 import {Container, Info} from './styles';
-import Button from '../../components/button/Button'
+import SideMenu from '../../components/sideMenu/SideMenu';
 
 
 const PageNotFound = () => {
@@ -12,31 +12,6 @@ const PageNotFound = () => {
                 <h3>We couldn't find that page!</h3>
                 <h2>Page Not Found</h2>
                 <h1>Tasky</h1>
-
-                <Button 
-                    setColor={"var(--red)"}
-                    setSize={"small"}
-                    click={()=>console.log("small")}                    
-                    >small
-                </Button>
-
-                <Button 
-                    setColor={"var(--red)"}
-                    setSize={"medium"}
-                    click={()=>console.log("small")}                    
-                    >medium
-                </Button>
-
-                <Button 
-                    setColor={"var(--red)"}
-                    setSize={"large"}
-                    click={()=>console.log("large")}                    
-                    >large
-                </Button>
-
-
-
-
             </Info>
         </Container>
     )
