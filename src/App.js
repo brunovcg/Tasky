983b8c4ff3ef28a99ca4f76bb3845ef389969f85
Henderson <Henderson.f.p@hotmail.com>
2021-06-11 11:53:51 -0300

    para funcionar o popup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import GlobalStyle from './styles/globalStyles';
 import Routes from './routes/routes';
+import { useContext } from 'react';
+import { FormDataContext } from './providers/formData';
+import { PopUp } from './components/pop-up/PopUp';
 
 function App() {
+
+  const { formData } = useContext(FormDataContext);
+  console.log(formData);
 
   return (
     <div> 
       <GlobalStyle/>
-      <Routes/>
+      <PopUp title='new ativcti' form='new Goal' />
+      {/* <Routes/> */}
     </div>
   );
 }
