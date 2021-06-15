50ed86a89c98b2aa6ea912d791997f4d04a8d2a3
Henderson <Henderson.f.p@hotmail.com>
2021-06-15 09:31:54 -0300

    feature: feature is on working

--- src/pages/group/Goals/Goals.jsx
+++ src/pages/group/Goals/Goals.jsx
@@ -2,18 +2,20 @@ import { GoalsContainer } from "../styles"
 import {PieChart} from '../Charts/Charts'
 // import {data} from '../Graphic/data'
 
-const Goals = () => {
+const Goals = ({ title, difficulty, how_much_achieved }) => {
+
+    const hasToDo = 100 - how_much_achieved; 
 
     return(
         <GoalsContainer>
             <div className="infoContainer">
-                <h3>Title</h3>
-                <p>Dificult: <span>{"Easy"}</span></p>
-                <p className="doneContainer">Done: <span className="doneInfo">{10}%</span></p>
-                <p className="todoContainer">To-Do: <span className="todoInfo">{90}%</span></p>
+                <h3>{title}</h3>
+                <p>Dificult: <span>{difficulty}</span></p>
+                <p className="doneContainer">Done: <span className="doneInfo">{how_much_achieved}%</span></p>
+                <p className="todoContainer">To-Do: <span className="todoInfo">{hasToDo}%</span></p>
             </div>
             <div className="chartContainer">
-                <PieChart doneData={10} todoData={90}/>
+                <PieChart doneData={how_much_achieved} todoData={hasToDo}/>
             </div>
         </GoalsContainer>
     )
--- src/pages/group/Goals/GoalsList.jsx
+++ src/pages/group/Goals/GoalsList.jsx
@@ -1,42 +1,58 @@
 import Goals from './Goals';
 import Button from '../../../components/button/Button';
-// import {PopUp} from '../../../components/pop-up/PopUp' 
+import {PopUp} from '../../../components/pop-up/PopUp' 
 import { useState } from 'react';
+import { UserGoals } from './userGoals';
+import { useEffect } from 'react';
 
 const GoalsList = () => {
 
     const [goalPopUp, setGoalPopUp] = useState(false)
 
+    const [goalsList, setGoalsList] = useState();
+
+    const [buttonNewGoal, setButtonNewGoal] = useState();
 
     const handleNewGoal = () => {
         setGoalPopUp(!goalPopUp)
     }
 
-
+    useEffect( () => {
+        UserGoals(setGoalsList)
+    }, [])
 
     return(
-        <div className="groupGoals">
+        <>
+            <div className="groupGoals">
 
-                    <div className="goalTitle">   
-                        <h3>Goals</h3>
-                        <Button
-                            setSize={"large"}
-                            setColor={"var(--blue)"}   
-                            click={()=>console.log("teste ADD Goal")}
-                            >+ Goal
-                        </Button>
-                    </div>
-                    <div className="goalMain">
-                        <Goals/>
-                        <Goals/>     
-                        <Goals/>          
-                    </div>
+                        <div className="goalTitle">   
+                            <h3>Goals</h3>
+                            <Button
+                                setSize={"large"}
+                                setColor={"var(--blue)"}   
+                                click={() => setButtonNewGoal(true)}
+                                >+ Goal
+                            </Button>
+                        </div>
+                        <div className="goalMain">
+                            { goalsList && goalsList.map((el) => <Goals {...el} />) }
+                        </div>
 
-                    <div className="popUps">
+                        <div className="popUps">
 
-                    </div>
+                        </div>
 
-                </div>
+            </div>
+            { buttonNewGoal && 
+                <div>
+                    <PopUp>
+                        <input />
+                        <input />
+                        <input />
+                    </PopUp>
+                </div> 
+            }
+        </>
     )
 }
 
