66f60608cad83067c0bd874f274ea2f4d3a70894
Henderson <Henderson.f.p@hotmail.com>
2021-06-15 12:35:44 -0300

    feature: working on goals request

--- src/pages/group/Goals/GoalsList.jsx
+++ src/pages/group/Goals/GoalsList.jsx
@@ -1,9 +1,13 @@
 import Goals from './Goals';
 import Button from '../../../components/button/Button';
-import {PopUp} from '../../../components/pop-up/PopUp' 
+import {PopUp} from '../../../components/pop-up/PopUp';
 import { useState } from 'react';
 import { UserGoals } from './userGoals';
 import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import Input from '../../../components/Input/Input.jsx';
 
 const GoalsList = () => {
 
@@ -13,8 +17,17 @@ const GoalsList = () => {
 
     const [buttonNewGoal, setButtonNewGoal] = useState();
 
-    const handleNewGoal = () => {
+    const schema = yup.object().shape({
+        title: yup.string().required('This field is required'),
+        difficulty: yup.string().required('This field is required'),
+        how_much_achieved: yup.number().required('This field is required'),
+    })
+
+    const { register, handleSubmit, formState: { error } } = useForm({ resolver: yupResolver(schema) })
+
+    const handleNewGoal = (data) => {
         setGoalPopUp(!goalPopUp)
+        console.log(data)
     }
 
     useEffect( () => {
@@ -45,10 +58,10 @@ const GoalsList = () => {
             </div>
             { buttonNewGoal && 
                 <div>
-                    <PopUp>
-                        <input />
-                        <input />
-                        <input />
+                    <PopUp title='new Goal' onSubmit={handleSubmit(handleNewGoal)} >
+                        <input {...register('title')} />
+                        <input {...register('difficulty')} />
+                        <input {...register('how_much_achieved')} />
                     </PopUp>
                 </div> 
             }
