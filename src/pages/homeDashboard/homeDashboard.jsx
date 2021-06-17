277aa49f3e385bbb94bada6abe3f874789a0c6ee
Carlos Martorini <c7.silveira@gmail.com>
2021-06-17 20:03:51 -0300

    fix: conflict resolver

--- src/pages/homeDashboard/homeDashboard.jsx
+++ src/pages/homeDashboard/homeDashboard.jsx
@@ -1,15 +1,10 @@
 import {Container} from './styles';
 import Graphic from '../../assets/graphic.png';
+import { useState } from 'react';
+
 
 const HomeDashboard = () => {
 
-<<<<<<< HEAD
-    return (
-        <Container>
-            <h3>Welcome to your page NAME, ready to accomplish your next task?</h3>
-            <img src={Graphic} alt='graphic'/>
-        </Container>
-=======
 
 
   
@@ -31,7 +26,6 @@ const HomeDashboard = () => {
             </Container>
         }
         </>
->>>>>>> e194bf77344644dd9662e4e28e9ff285199c207b
     )
 }
 
