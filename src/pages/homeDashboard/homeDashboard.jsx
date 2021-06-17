0b1e90d29bec54f0b8713ccf5b7b3a34bf06ae7f
Carlos Martorini <c7.silveira@gmail.com>
2021-06-17 19:30:07 -0300

    fix: conflict resolver

--- src/pages/homeDashboard/homeDashboard.jsx
+++ src/pages/homeDashboard/homeDashboard.jsx
@@ -1,29 +1,13 @@
 import {Container} from './styles';
 import Graphic from '../../assets/graphic.png';
-import { useLoginData } from '../../providers/login';
-import { useState } from 'react';
 
 const HomeDashboard = () => {
 
-    const { userLoginData, setUserLoginData } = useLoginData();
-
-    const [username, setUsername] = useState();
-
-    setUserLoginData['username'] = setUsername
-
-    console.log(userLoginData);
-    console.log(username);
-
     return (
-        <>
-        {
-            // username &&
-            <Container>
-                <h3>Welcome to your page NAME, ready to accomplish your next task?</h3>
-                <img src={Graphic} alt='graphic'/>
-            </Container>
-        }
-        </>
+        <Container>
+            <h3>Welcome to your page NAME, ready to accomplish your next task?</h3>
+            <img src={Graphic} alt='graphic'/>
+        </Container>
     )
 }
 
