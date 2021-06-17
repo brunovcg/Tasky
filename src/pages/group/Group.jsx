5267db842b2f50dcbcc6c4ec96318f73e10d2262
eduardofernandes2103 <eduardofernandes21031@gmail.com>
2021-06-17 19:40:19 -0300

    fix: Activities Cards

--- src/pages/group/Group.jsx
+++ src/pages/group/Group.jsx
@@ -2,12 +2,21 @@ import GoalsList from './Goals/GoalsList'
 import ActivitiesList from './Activities/ActivitiesList'
 import {GroupContainer} from './styles';
 import Button from '../../components/button/Button'
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import { useWindowSize } from '../../providers/windowSize';
+import { useParams } from 'react-router-dom'
+import { api } from '../../service/api';
+
 
 
 
 const Group = () => {
+
+    const params = useParams()
+
+    const userToken = JSON.parse( localStorage.getItem('@tasky/login/token') );
+
+    const [specifGroup, setSpecificGroup] = useState([])
 
     const [hidden, setHidden] = useState(true);
 
@@ -18,6 +27,21 @@ const Group = () => {
     const changeDisplay = () => {
         setHidden(!hidden)
     }
+
+    const groupRequest = () => {
+        api
+            .get(`/groups/${params.id}/`, {
+                headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userToken}`,
+                },
+            })
+            .then(response => setSpecificGroup(response.data))
+    }
+
+    useEffect(() => {
+        groupRequest();
+      }, []);
 
 
     return(
@@ -44,7 +68,7 @@ const Group = () => {
                  { width > 500 ? 
                     <>
                         <GoalsList/>
-                        <ActivitiesList/>
+                        <ActivitiesList specifGroup={specifGroup}/>
                     </>
                     
                     : ( hidden ? <ActivitiesList/> : <GoalsList/>)
